dd97468e688a2eba97dd29f63d8c47d55a61af20
PipeManMusic <PipeManMusic@gmail.com>
2010-06-29 21:12:08 -0500

    fixed comment tags

--- geostatus.py
+++ geostatus.py
@@ -8,8 +8,9 @@
 ###    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ###    GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###    You should have received a copy of the GNU General Public License
+###    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import android, math, xmpp
 from time import sleep
 droid = android.Android()
